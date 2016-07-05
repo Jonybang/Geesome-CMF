def3534d877a1296c60acfb91c7bb806f9b590dd
Jonybang <jonybang@mail.ru>
2016-07-05 12:27:19 +1000

    date and textarea sub fields

--- public/assets/dist/app.js
+++ public/assets/dist/app.js
@@ -1415,6 +1415,32 @@ angular.module('app')
     }]);
 angular
     .module('app')
+    .directive('sfDate', ['$timeout', 'AppPaths', function($timeout, AppPaths) {
+        return {
+            restrict: 'E',
+            templateUrl: AppPaths.app + 'directives/sf-date.html',
+            scope: {
+                /* SubFieldValues resource */
+                ngModel: '=',
+                pageResource: '=?',
+                templateResource: '=?'
+            },
+            link: function (scope, element) {
+                scope.$watch('ngModel', function(){
+                    if(!scope.ngModel || !scope.ngModel.value)
+                        return;
+
+                    if(new Date(scope.ngModel.value) != scope.fakeModel)
+                        scope.fakeModel = new Date(scope.ngModel.value);
+                });
+                scope.$watch('fakeModel', function(){
+                    scope.ngModel.value = scope.fakeModel;
+                });
+            }
+        };
+    }]);
+angular
+    .module('app')
     .directive('sfImage', ['$timeout', 'AppPaths', function($timeout, AppPaths) {
         return {
             restrict: 'E',
@@ -1499,6 +1525,23 @@ angular
         return {
             restrict: 'E',
             templateUrl: AppPaths.app + 'directives/sf-text.html',
+            scope: {
+                /* SubFieldValues resource */
+                ngModel: '=',
+                pageResource: '=?',
+                templateResource: '=?'
+            },
+            link: function (scope, element) {
+
+            }
+        };
+    }]);
+angular
+    .module('app')
+    .directive('sfTextarea', ['$timeout', 'AppPaths', function($timeout, AppPaths) {
+        return {
+            restrict: 'E',
+            templateUrl: AppPaths.app + 'directives/sf-textarea.html',
             scope: {
                 /* SubFieldValues resource */
                 ngModel: '=',
