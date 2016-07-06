ece1b21e35597f2f2c5a8caa8bfec7f1062bcfa1
Jonybang <jonybang@mail.ru>
2016-07-06 17:47:57 +1000

    header move to layout and create form template

--- resources/views/layouts/client.blade.php
+++ resources/views/layouts/client.blade.php
@@ -49,16 +49,14 @@
 @endsection
 
 @section('header')
-    <!-- Page Header -->
-    <!-- Set your background image for this header on the line below. -->
-    <header class="intro-header" style="background-image: url('assets/img/home-bg.jpg')">
+    <header class="intro-header" style="background-image: url('{{isset($sf['imageLink']) && $sf['imageLink'] ? $sf['imageLink'] : 'assets/img/home-bg.jpg'}}')">
         <div class="container">
             <div class="row">
                 <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
                     <div class="site-heading">
-                        <h1>Lanit Dev</h1>
+                        <h1>{{$page->title}}</h1>
                         <hr class="small">
-                        <span class="subheading">Developer team of Lanit Company.</span>
+                        <span class="subheading">{{$page->sub_title}}</span>
                     </div>
                 </div>
             </div>
--- resources/views/templates/index.blade.php
+++ resources/views/templates/index.blade.php
@@ -2,21 +2,6 @@
 
 @section('title', $page->title)
 
-@section('header')
-    <header class="intro-header" style="background-image: url('{{$sf['imageLink'] or 'assets/img/home-bg.jpg'}}')">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-                    <div class="site-heading">
-                        <h1>{{$page->title}}</h1>
-                        <hr class="small">
-                        <span class="subheading">{{$page->sub_title}}</span>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </header>
-@endsection
 
 @section('content')
     @foreach($page->child_pages_by_index as $block)
