0922f19e0fdcc67d4a1be2c8d8150dee6520fe30
Jonybang <jonybang@mail.ru>
2016-07-15 11:17:07 +1000

    cancel session expire

--- config/auth.php
+++ config/auth.php
@@ -100,7 +100,7 @@ return [
             'provider' => 'users',
             'email' => 'auth.emails.password',
             'table' => 'password_resets',
-            'expire' => 25200,
+            'expire' => 60,
         ],
     ],
 
