7ede17810714437c7f2b0f69013ff1588d914b46
Jonybang <jonybang@mail.ru>
2016-08-09 17:58:22 +1000

    delete debug info

--- resources/views/templates/index.blade.php
+++ resources/views/templates/index.blade.php
@@ -4,9 +4,7 @@
 
 
 @section('content')
-    lala
     @foreach($page->child_pages_by_index as $block)
-        lala
         @include('templates.' . $block->template->key)
     @endforeach
 @endsection
