23a5547959a3275fc3376240c65ddf983aa31b96
Jonybang <jonybang@mail.ru>
2016-09-05 10:41:30 +1000

    robots, sitemap and google analytics on client side

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,6 +14,50 @@ use App\Models\Context;
 */
 
 //========================================================================================================
+// ROBOTS.TXT
+//========================================================================================================
+
+Route::get('robots.txt', function ()
+{
+    if (App::environment() == 'production') {
+        // If on the live server, serve a nice, welcoming robots.txt.
+        Robots::addUserAgent('*');
+        Robots::addSitemap('sitemap.xml');
+    } else {
+        // If you're on any other server, tell everyone to go away.
+        Robots::addDisallow('*');
+    }
+
+    return Response::make(Robots::generate(), 200, ['Content-Type' => 'text/plain']);
+});
+
+//========================================================================================================
+// SITEMAP
+//========================================================================================================
+
+Route::get('sitemap.xml', function(){
+
+    // create new sitemap object
+    $sitemap = App::make("sitemap");
+
+    // set cache key (string), duration in minutes (Carbon|Datetime|int), turn on/off (boolean)
+    // by default cache is disabled
+    $sitemap->setCache('laravel.sitemap', 60);
+
+    // check if there is cached sitemap and build new only if is not
+    if (!$sitemap->isCached())
+    {
+        $pages = Page::where(['is_published' => true, 'is_deleted' => false, 'is_part' => false])->get();
+        foreach ($pages as $page) {
+            $sitemap->add($page->alias, $page->updated_at, '1.0', 'daily');
+        }
+    }
+    // show your sitemap (options: 'xml' (default), 'html', 'txt', 'ror-rss', 'ror-rdf')
+    return $sitemap->render('xml');
+
+});
+
+//========================================================================================================
 // CUSTOM ACTIONS
 //========================================================================================================
 
