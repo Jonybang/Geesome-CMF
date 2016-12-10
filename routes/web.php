d354dd52932de77883402d44201789fb9be24342
Jonybang <jonybang@mail.ru>
2016-12-11 06:49:22 +1000

    request params

--- routes/web.php
+++ routes/web.php
@@ -219,7 +219,8 @@ Route::group(['prefix' => LaravelLocalization::setLocale()], function(){
                 $result = \App::call('App\\Http\\Controllers\\' . $controller_action->key, [
                     'page' => $page,
                     'url_query' => $url_query,
-                    'last_alias' => isset($url_query_array) ? $url_query_array[count($url_query_array) - 1] : null
+                    'last_alias' => isset($url_query_array) ? $url_query_array[count($url_query_array) - 1] : null,
+					'params' => $this->app->request->all()
                 ]);
 
                 if($result instanceof \Illuminate\Http\JsonResponse)
